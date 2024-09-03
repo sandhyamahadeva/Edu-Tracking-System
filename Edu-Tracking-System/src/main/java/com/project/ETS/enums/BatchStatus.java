a64e222ebc1b1d7951f5bfbbd9f0c93aadf93a16
sandhyamahadeva <150340770+sandhyamahadeva@users.noreply.github.com>
2024-09-03 12:11:24 +0530

    Delete Edu-Tracking-System directory

--- Edu-Tracking-System/src/main/java/com/project/ETS/enums/BatchStatus.java
+++
@@ -1,7 +0,0 @@
-package com.project.ETS.enums;
-
-public enum BatchStatus {
-
-	CREATED,ON_GOING,CANCELLED,CLOSED;
-
-}
--- Edu-Tracking-System/src/main/java/com/project/ETS/enums/Subject.java
+++
@@ -1,6 +0,0 @@
-package com.project.ETS.enums;
-
-public enum Subject {
-
-	SQL,JAVA,MONGO_DB,CSS,HTML,JAVA_SCRIPT,HIBERNATE,REACT_JS,JDBC,SERVLETS,SPRING,NODE_JS;
-}
