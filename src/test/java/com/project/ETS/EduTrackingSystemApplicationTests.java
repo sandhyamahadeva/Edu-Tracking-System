e1a5c54e84958d0d52b5e25d33df27318d17dd4c
sandhyamahadeva <150340770+sandhyamahadeva@users.noreply.github.com>
2024-09-03 13:22:14 +0530

    Update EduTrackingSystemApplicationTests.java

--- src/test/java/com/project/ETS/EduTrackingSystemApplicationTests.java
+++ src/test/java/com/project/ETS/EduTrackingSystemApplicationTests.java
@@ -3,6 +3,7 @@ package com.project.ETS;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class EduTrackingSystemApplicationTests {
 
